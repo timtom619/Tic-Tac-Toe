798fb2576824460e9b341b24d3db70a45623af08
Timothy Tomas <timothyjtee@gmail.com>
2023-01-11 11:57:58 -0800

    implement displaying player choice into square

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// // test the module
-// myModule.increment();
-// myModule.increment();
-// console.log(myModule.getCount()); // 2
-
 //starting game board
 const GameBoard = (() => {
   const gameBoard = new Array(9);
@@ -18,15 +13,39 @@ const GameBoard = (() => {
 
 // Player should have:
 // name, choice 'x' or 'o'
-const player = (name, choice) => {
-  return{ name, choice,}
+const player = (choice) => {
+  return {choice}
 }
 
 // Object to store the flow of the game
 const displayController = (() => {
-  let counter = 1;
-  const updatedBoard = (position, choice) => {
-    gameBoard.splice(position, 0, choice);
-    return gameBoard;
+  return {
+    // getChoice: () => choice,
+    // setChoice: (value) => choice = value
   }
-})();
+})
+
+// When user clicks on box
+   // add a event listener on each square
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+  square.addEventListener('click', updateValue);
+});
+
+// Should have a way of knowing if player is X or O
+const btnX = document.querySelector('.btn.x');
+let playerOne;
+btnX.addEventListener('click', (event) => {
+  playerOne = player(event.target.innerText);
+  console.log(playerOne.choice);
+});
+// then add the players value into that position
+function updateValue(event) {
+  if(playerOne) {
+    event.target.innerText = playerOne.choice;
+  }
+}
+
+// const children = document.getElementsByTagName('span');
+// children[0].innerHTML = 'H';
+// console.log(children[0].innerHTML);
